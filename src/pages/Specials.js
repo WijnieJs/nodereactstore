a63f2eb176abb5b0193c35459020c4c71a0842b4
wijnandK <korlaarwijnand@hotmail.com>
2021-10-16 14:19:23 +0200

    Tryout

--- src/pages/Specials.js
+++ src/pages/Specials.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Specials = () => {
   return (
-    <div>
-      <h1> SPECIALL US </h1>
+    <div className="background">
+      <div className="special3d">
+        <div className="line-1"></div>
+        <div className="line-2"></div>
+        <div className="line-3"></div>
+      </div>
+      <div className="special1d">
+        {/* <div className="line-4"></div>
+        <div className="line-5"></div>
+        <div className="line-6"></div> */}
+      </div>
     </div>
   );
-}
+};
 
 export default Specials;
